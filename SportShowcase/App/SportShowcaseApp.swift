957ce184eb46bd71a32e2eb9a9560ae82e0e8360
Julio Cesar Reyes <jcreyesdev@gmail.com>
2026-06-01 23:12:54 +0200

    chore: app entry point placeholder

--- SportShowcase/App/SportShowcaseApp.swift
+++ SportShowcase/App/SportShowcaseApp.swift
@@ -1,17 +1,10 @@
-//
-//  SportShowcaseApp.swift
-//  SportShowcase
-//
-//  Created by Julio César Reyes on 01/06/2026.
-//
-
 import SwiftUI
 
 @main
 struct SportShowcaseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("SportShowcase")
         }
     }
 }
