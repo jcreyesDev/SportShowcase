f32daf6e6514788b4734c0e86e7b311677ddbfaa
Julio Cesar Reyes <jcreyesdev@gmail.com>
2026-06-01 19:25:52 +0200

    Initial Commit

---
+++ SportShowcase/SportShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportShowcaseApp.swift
+//  SportShowcase
+//
+//  Created by Julio César Reyes on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SportShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
